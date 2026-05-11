648507450e0beb52b71bdaea574c688abd034317
sudocbrown99 <cmichaelbrown99@gmail.com>
2026-05-11 17:47:05 -0500

    Add command struct and map of supported commands.

--- repl.go
+++ repl.go
@@ -6,11 +6,27 @@ import (
 	"strings"
 )
 
+// Command structure
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commandList = map[string]cliCommand{
+	"exit": {
+		name:        "exit",
+		description: "Exits the Pokedex",
+		callback:    commandExit,
+	},
+}
+
 // Takes input, removes whitespace and converts to lowercase
 func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(text))
 }
 
+// Exits program
 func commandExit() error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
