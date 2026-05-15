46c1320c61011921a105337361bf9a3a6b284799
sudocbrown99 <cmichaelbrown99@gmail.com>
2026-05-15 11:21:10 -0500

    feat: reorganize file structure

--- main.go
+++ main.go
@@ -1,31 +1,16 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
+	"time"
+
+	"github.com/cm-brown/pokedex/internal/pokeapi"
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	// Start REPL
-	for {
-		fmt.Println("Pokedex >")
-		scanner.Scan()
-		line := scanner.Text()
-		cleanLine := cleanInput(line)
-
-		// Check if user command exists
-		cmd, ok := getCommands()[cleanLine[0]]
-		if !ok {
-			fmt.Println("Unknown command:", cleanLine[0])
-			continue
-		}
-		// Executes command callback
-		err := cmd.callback()
-		if err != nil {
-			fmt.Println("Error executing command:", err)
-		}
+	pokeClient := pokeapi.NewClient(5 * time.Second)
+	cfg := &config{
+		pokeapiClient: pokeClient,
 	}
+
+	startRepl(cfg)
 }
--- repl.go
+++ repl.go
@@ -1,96 +1,79 @@
 package main
 
 import (
-	"encoding/json"
+	"bufio"
 	"fmt"
-	"io"
-	"log"
-	"net/http"
 	"os"
 	"strings"
+
+	"github.com/cm-brown/pokedex/internal/pokeapi"
 )
 
-// Command structure
+type config struct {
+	pokeapiClient    pokeapi.Client
+	nextLocationsURL *string
+	prevLocationsURL *string
+}
+
+func startRepl(cfg *config) {
+	reader := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		reader.Scan()
+
+		words := cleanInput(reader.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		commandName := words[0]
+
+		command, exists := getCommands()[commandName]
+		if exists {
+			err := command.callback(cfg)
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
+	}
+}
+
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*config) error
 }
 
 func getCommands() map[string]cliCommand {
 	return map[string]cliCommand{
-		"exit": {
-			name:        "exit",
-			description: "Exits the Pokedex",
-			callback:    commandExit,
-		},
 		"help": {
 			name:        "help",
-			description: "Displays a list of available commands",
+			description: "Displays a help message",
 			callback:    commandHelp,
 		},
 		"map": {
 			name:        "map",
-			description: "Displays the names of 20 location areas in the Pokemon world",
-			callback:    commandMap,
+			description: "Get the next page of locations",
+			callback:    commandMapf,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Get the previous page of locations",
+			callback:    commandMapb,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
 		},
 	}
-}
-
-// LocationArea structure
-type LocationArea struct {
-	Name string `json:"name"`
-	Url  string `json:"url"`
-}
-type LocationAreasResponse struct {
-	Count    int            `json:"count"`
-	Next     string         `json:"next"`
-	Previous string         `json:"previous"`
-	Results  []LocationArea `json:"results"`
 }
 
 // Takes input, removes whitespace and converts to lowercase
 func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(text))
-}
-
-// Exits program
-func commandExit() error {
-	fmt.Println("Closing the Pokedex... Goodbye!")
-	os.Exit(0)
-	return nil
-}
-
-// Prints a list of available commands
-func commandHelp() error {
-	commands := getCommands()
-	fmt.Println("Welcome to the Pokedex!")
-	fmt.Println("Usage:\n")
-	for _, cmd := range commands {
-		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
-	}
-	return nil
-}
-
-// Prints a list of location areas
-func commandMap() error {
-	res, err := http.Get("https://pokeapi.co/api/v2/location-area/")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	response := LocationAreasResponse{}
-	if err := json.Unmarshal([]byte(body), &response); err != nil {
-		log.Fatal(err)
-	}
-
-	for _, area := range response.Results {
-		fmt.Println(area.Name)
-	}
-	return nil
 }
